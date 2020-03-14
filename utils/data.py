b800983c02a2252a5abb08aca120bdd7515c93e9
Michał Jaworski <jawor403@gmail.com>
2020-03-14 18:11:13 +0100

    Change data_utils to utils.data

--- utils/data.py
+++ utils/data.py
@@ -1,17 +1,22 @@
-from keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from os import path
 import pandas as pd
+from typing import Tuple
 
 
 DATA_ROOT = "data"
 
 
 def get_data_loaders(
-        train_gen: image.ImageDataGenerator,
-        test_gen: image.ImageDataGenerator,
+        train_gen: ImageDataGenerator,
+        test_gen: ImageDataGenerator = None,
         img_root: str = path.join(DATA_ROOT, "SUN397"),
+        target_size: Tuple[int, int] = (256, 256),
         **kwargs):
     loaders = {}
+    if test_gen is None:
+        test_gen = ImageDataGenerator()
+
     for subset in ["train", "valid", "test"]:
         df = pd.read_csv(path.join(DATA_ROOT, f"sun_{subset}.csv"))
         data_gen = train_gen if subset == "train" else test_gen
@@ -20,6 +25,7 @@ def get_data_loaders(
             directory=img_root,
             x_col="path",
             y_col="label",
+            target_size=target_size,
             **kwargs
         )
     return loaders
