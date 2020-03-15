1ba757be0b00dcb608b7345c8257f4849f3ed8eb
Michał Jaworski <jawor403@gmail.com>
2020-03-15 16:36:26 +0100

    Add scripts/download.sh for downloading data to a specified directory

---
+++ scripts/download.sh
@@ -0,0 +1,5 @@
+DATA_URL=http://vision.princeton.edu/projects/2010/SUN/SUN397.tar.gz
+path=${1:-data}
+
+echo "Extracting image data to $(readlink -f "$path")"
+wget -nv --show-progress -O - $DATA_URL | tar -xzC "$path"
