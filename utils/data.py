426c31256affc4b31ad881525a4a1d4ff7af18de
Michał Jaworski <jawor403@gmail.com>
2020-03-15 15:38:11 +0100

    Add exception on missing datasets
    Better variable naming

--- utils/data.py
+++ utils/data.py
@@ -10,22 +10,25 @@ DATA_ROOT = "data"
 def get_data_loaders(
         train_gen: ImageDataGenerator,
         test_gen: ImageDataGenerator = None,
-        img_root: str = path.join(DATA_ROOT, "SUN397"),
-        target_size: Tuple[int, int] = (256, 256),
+        directory: str = path.join(DATA_ROOT, "SUN397"),
+        image_shape: Tuple[int, int] = (256, 256),
         **kwargs):
     loaders = {}
     if test_gen is None:
         test_gen = ImageDataGenerator()
 
     for subset in ["train", "valid", "test"]:
-        df = pd.read_csv(path.join(DATA_ROOT, f"sun_{subset}.csv"))
+        df = pd.read_csv(path.join(DATA_ROOT, f"sun_{subset}.csv")).set_index("id")
         data_gen = train_gen if subset == "train" else test_gen
         loaders[subset] = data_gen.flow_from_dataframe(
             dataframe=df,
-            directory=img_root,
+            directory=directory,
             x_col="path",
             y_col="label",
-            target_size=target_size,
+            target_size=image_shape,
             **kwargs
         )
+
+    if not all(loaders.values()):
+        raise RuntimeError("Image data missing. Image directory: " + directory)
     return loaders
