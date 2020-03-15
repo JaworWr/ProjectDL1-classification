4c688df19c50041993e50b587aa356a1baba10be
Michał Jaworski <jawor403@gmail.com>
2020-03-15 19:08:55 +0100

    Add a trainer base class

---
+++ base/trainer.py
@@ -0,0 +1,18 @@
+class BaseTrainer:
+    def __init__(self, model, data, config):
+        self.model = model
+        self.data = data
+        self.config = config
+
+        self.callbacks = []
+
+    def init_callbacks(self):
+        pass
+
+    def train(self):
+        self.model.fit(
+            self.data["train"],
+            validation_data=self.data["valid"],
+            callbacks=self.callbacks,
+            **self.config["training"]
+        )
