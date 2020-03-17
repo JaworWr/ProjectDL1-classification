c940ba49261cd2e63a3a1995dc618b5f89e9c8c8
Michał Jaworski <jawor403@gmail.com>
2020-03-17 13:56:51 +0100

    Add tensorboard callback

--- base/trainer.py
+++ base/trainer.py
@@ -1,3 +1,8 @@
+from tensorflow.keras import callbacks
+import os
+from datetime import datetime
+
+
 class BaseTrainer:
     def __init__(self, config, model, data):
         self.config = config
@@ -8,6 +13,12 @@ class BaseTrainer:
 
     def init_callbacks(self):
         pass
+
+    def init_tensorboard_callback(self, experiment_name: str = None, directory: str = "experiments/logs"):
+        if experiment_name is None:
+            experiment_name = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        tensorboard_callback = callbacks.TensorBoard(log_dir=os.path.join(directory, experiment_name))
+        self.callbacks.append(tensorboard_callback)
 
     def train(self):
         self.model.fit(
