e1c6b1e3f91ca93475875e09fba65c51378dfb62
Michał Jaworski <jawor403@gmail.com>
2020-03-18 20:02:51 +0100

    Tensorflow 2.0 compatibility

--- utils/devices.py
+++ utils/devices.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 def device_setup(config):
     if config.devices.get("log_device_placement", False):
         tf.debugging.set_log_device_placement(True)
-    devices = tf.config.list_physical_devices("GPU")
+    devices = tf.config.experimental.list_physical_devices("GPU")
     if devices:
         print("GPU devices found")
         if config.devices.get("memory_growth", False):
