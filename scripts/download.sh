1d79be4393e3e5ed56c553718819a6f2c191184a
Michał Jaworski <jawor403@gmail.com>
2020-03-27 20:06:11 +0100

    Create target directory if it doesn't exist

--- scripts/download.sh
+++ scripts/download.sh
@@ -6,4 +6,5 @@ DATA_URL=http://vision.princeton.edu/projects/2010/SUN/SUN397.tar.gz
 path=${1:-data}
 
 echo "Extracting image data to $(readlink -f "$path")"
+mkdir -p $path
 wget -nv --show-progress -O - $DATA_URL | tar -xzC "$path"
